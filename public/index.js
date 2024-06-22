ccca13c57a2b4d5e87e9a77d01319750e87e1a70
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-06-22 15:36:29 -0700

    Finish activity

--- public/index.js
+++ public/index.js
@@ -3,7 +3,58 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    const apiUrl = 'https://api.twelvedata.com/time_series?apikey=905eea2913094163ba179aa1d9a15f28&interval=1day&symbol=TSLA&outputsize=1'
 
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }    
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+        stocks.forEach( stock => stock.values.reverse())
+
+        new Chart(timeChartCanvas.getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: stocks[0].values.map(value => value.dateTime),
+                datasets: stocks.map( stock => ({
+                    label: stock.meta.symbol,
+                    data: stock.values.map(value => parseFloat(value.high)),
+                    backgroundColor: getColor(stock.meta.symbol),
+                    borderColor: getColor(stock.meta.symbol),
+                }))
+            }
+        });
+        new Chart(highestPriceChartCanvas.getContext('2d'), {
+            type: 'bar',
+            data: {
+                labels: stocks[0].values.map(value => value.symbol),
+                datasets: [65, 59, 80, 81, 56, 55, 40], 
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }
+        })
 }
 
 main()
